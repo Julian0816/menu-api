53ddbdd3d6374980f084dbaa7a2714c8b5cfde65
Julian Martinez <julian.martinez.cs@gmail.com>
2022-09-12 13:14:10 +0100

    created methods findAll, find, create, update, remove

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -37,3 +37,64 @@ let items: Items = {
  * Service Methods
  */
 
+//Simulating asyncronous nature of read and write operations
+export const findAll = async (): Promise<Item[]> => Object.values(items);
+export const find = async (id: number): Promise<Item> => items[id];
+
+
+//Method to create a new item in the store
+export const create = async (newItem: BaseItem) => {
+    const id = new Date().valueOf();
+
+    items[id] = {
+        id,
+        ...newItem,
+    };
+
+    return items[id]
+}
+/**
+ * The create method receives an object of type BaseItem as an argument,
+ * providing all the required values to define a new item in the store, except the item's id.
+ * To create a unique id value for each new element added to the store, you use the value
+ * of the current Date, based on the number of milliseconds between 1 January 1970
+ * 00:00:00 UTC and the current time.
+ */
+
+
+//Method to update an existing store item
+export const update = async (
+    id: number,
+    itemUpdate: BaseItem
+): Promise<Item | null> => {
+    const item = await find(id);
+
+    if(!item) {
+        return null;
+    }
+
+    items[id] = {id, ...itemUpdate};
+
+    return items[id];
+};
+/**
+ * The update method receives the item id property and an itemUpdate object as
+ * arguments. You use the id to find the item in the store to update it wit the properties of
+ * itemUpdate. If the store doesn't have the item, you return null.
+ */
+
+
+//Method to remove an item from the store
+export const remove = async (id: number): Promise<null | void> => {
+    const item = await find(id);
+
+    if(!item) {
+        return null;
+    }
+
+    delete items[id];
+};
+/**
+ * The remove method receives an id value as a parameter and uses it to look up an item
+ * in the store and to delete it if found.
+ */
