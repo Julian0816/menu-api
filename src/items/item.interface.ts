daa85e50bad232654123024a8e23aa3f0a4f31e5
Julian Martinez <julian.martinez.cs@gmail.com>
2022-09-12 11:41:50 +0100

    BaseItem and Item interfaces created

---
+++ src/items/item.interface.ts
@@ -0,0 +1,19 @@
+
+
+export interface BaseItem {
+    name: string;
+    price: number;
+    decription: string;
+    img: string;
+}
+
+export interface Item extends BaseItem {
+    id: number;
+}
+
+/**
+ * There will be scenarios where you only need to assert the item's structure without its id,
+ * such as when you get a payload to create a new item or update an existing one. In those
+ * types of requests, POST and PUT, the client sends the item id as a query parameter. For
+ * those cases, you'll use the BaseItem interface.
+ */
